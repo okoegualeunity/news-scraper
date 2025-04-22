28213119b70c66afe96ef54e4799a6192511828a
okoegualeunity <150802108+okoegualeunity@users.noreply.github.com>
2025-04-22 09:31:42 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/scrape-news')
+def scrape_news():
+    url = 'https://www.bbc.com/news/technology'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    articles = []
+    for item in soup.select('a.gs-c-promo-heading'):
+        title = item.get_text()
+        link = item['href']
+        if link.startswith('/'):
+            link = 'https://www.bbc.com' + link
+        articles.append({'title': title, 'url': link})
+
+    return jsonify(articles)
